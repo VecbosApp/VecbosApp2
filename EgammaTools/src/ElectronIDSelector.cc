eecb2df0219ef1748cf1a5416af7f5a5c4669677
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2013-12-31 01:33:30 +0100

    some trivial fixes

--- EgammaTools/src/ElectronIDSelector.cc
+++ EgammaTools/src/ElectronIDSelector.cc
@@ -27,11 +27,11 @@ ElectronCollection ElectronIDSelector::output() {
   else if(algo_.compare("cuts_ip")==0)      return output_cuts_ip();
   else if(algo_.compare("cuts")==0)         return output_cuts();
 
-  else if(algo_.compare("mva_id")==0)      return output_cuts_id();
-  else if(algo_.compare("mva_iso")==0)     return output_cuts_iso();
-  else if(algo_.compare("mva_convrej")==0) return output_cuts_convrej();
-  else if(algo_.compare("mva_ip")==0)      return output_cuts_ip();
-  else if(algo_.compare("mva")==0)         return output_cuts();
+  else if(algo_.compare("mva_id")==0)      return output_mva_id();
+  else if(algo_.compare("mva_iso")==0)     return output_mva_iso();
+  else if(algo_.compare("mva_convrej")==0) return output_mva_convrej();
+  else if(algo_.compare("mva_ip")==0)      return output_mva_ip();
+  else if(algo_.compare("mva")==0)         return output_mva();
 
   else cout << "ElectronIDSelector: algo " << algo_ << " is not foreseen, returning a null collection..." << endl;
   return output_;
@@ -131,11 +131,13 @@ ElectronCollection ElectronIDSelector::output_mva_ip() {
 
 ElectronCollection ElectronIDSelector::output_mva() {
   output_.clear();
+  int iele=0;
   for(electron_iterator ele=input_.begin(); ele!=input_.end(); ++ele) {
     ElectronIDAlgo algo(*ele,rho_);
     if(algo.pass_mva_id(algo_,wp_) && algo.pass_mva_iso(wp_) && 
        algo.pass_mva_convrej(wp_) && algo.pass_mva_ip(wp_)
        ) output_.push_back(*ele);
+    ++iele;
   }
   return output_;
 }
