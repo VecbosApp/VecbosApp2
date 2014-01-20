e7ffeb237522545c3da4b75e066dd84507a81dab
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2014-01-20 19:12:12 +0100

    added the Z(ll) + 1 electron selection for the electron ID studies using vecbos2

---
+++ EgammaTools/include/FakeElectronSelectorZllPlusOneFake.hh
@@ -0,0 +1,50 @@
+///---------------------------------------
+// Description:
+// Class to fill a tree of fake electron candidates to study ele ID
+//---------------------------------------
+
+#ifndef FAKE_ELECTRON_SELECTOR_ZLL_PLUS_ONE_FAKE_H
+#define FAKE_ELECTRON_SELECTOR_ZLL_PLUS_ONE_FAKE_H
+
+#include "Analysis/include/AnalysisBase.hh"
+#include "Tools/include/HLTFilter.hh"
+#include "EgammaTools/include/ElectronIDSelector.hh"
+#include "MuonTools/include/MuonIDSelector.hh"
+#include "OutputTrees/include/ElectronIDTree.hh"
+
+namespace vecbos {
+  
+  class FakeElectronSelectorZllPlusOneFake : public AnalysisBase {
+
+  public:
+    //! constructor
+    FakeElectronSelectorZllPlusOneFake(TChain *tree=0);
+    //! destructor
+    virtual ~FakeElectronSelectorZllPlusOneFake() { }
+    
+    //! set the dataset (DoubleMuon or DoubleElectron)
+    void setDataset(std::string dataset) { dataset_ = dataset; }
+    
+    //! configure the needed stuff
+    void BeginJob(JobConfiguration *conf);
+    //! close the needed stuff
+    void EndJob();
+    //! loop over events
+    void Loop();
+    
+  private:
+    
+    /// data members
+    ElectronIDSelector elid_mva_loose;
+    MuonIDSelector muonid_loose;
+    HLTFilter *doubleele_filter_8TeV, *doublemu_filter_8TeV;
+    ElectronIDTree *output;
+    std::string dataset_;
+    
+    // fill the tree for the selected Z(ll)+1 electron
+    void fillProbe(Electron *electron, float zmass, int zdecay);
+
+  };
+}
+
+#endif
