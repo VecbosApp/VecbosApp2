45a89f80547376d08b68e4754c0bf18a9702e61e
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2014-02-03 19:10:09 +0100

    a script to run the fr

---
+++ EgammaTools/macro/runfr.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import sys
+import os
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-r", "--recompile",
+                  action="store_true", dest="recompile", default=False,
+                  help="recompile the fake rate estimator")
+parser.add_option("-d", "--drawonly",
+                  action="store_true", dest="drawonly", default=False,
+                  help="do not run. Only draw the results based on existing ROOT files")
+
+(options, args) = parser.parse_args()
+
+if(options.drawonly == False):
+    if(options.recompile):
+        print 'recompiling...'
+        os.system('make clean')
+        os.system('make fakerate')
+        print 'done complining.'
+
+    if(options.electrons == True):
+        os.system('./fakerate fakes-zll1e fr')
+        os.system('hadd -f fakerates_nontrg.root fr-EleMisid*')
+        os.system('rm -f fr-EleMisid*.root')
+
+from ROOT import gROOT
+gROOT.LoadMacro('drawFR.cc+')
+if(options.electrons == True):
+    from ROOT import drawIdsBiased
+    drawIdsBiased()
+
